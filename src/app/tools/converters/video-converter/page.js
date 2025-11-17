d14a03f580c8cdb73e97ccbeea09fe727aba569e
yngdrg <bellaroger691@gmail.com>
2025-11-17 16:16:57 +0500

    Fix folder structure for VideoConverter

--- src/app/tools/converters/video-converter/page.js
+++ src/app/tools/converters/video-converter/page.js
@@ -1,10 +1,9 @@
-// src/app/tools/converters/video-converter/page.js
 import dynamic from "next/dynamic";
 
 export default function VideoConverterPage() {
   const VideoConverter = dynamic(
-    () => import("./VideoConverterClient"), // load client-only component
-    { ssr: false } // CRUCIAL: disables server-side rendering
+    () => import("./VideoConverterClient"),
+    { ssr: false }
   );
 
   return <VideoConverter />;
