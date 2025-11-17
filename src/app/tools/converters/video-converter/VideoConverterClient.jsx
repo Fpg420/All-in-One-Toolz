3e4ffb15eff41de0e9d74780d4af9b5e5384b386
yngdrg <bellaroger691@gmail.com>
2025-11-17 16:55:11 +0500

    Add VideoConverterClient component

---
+++ src/app/tools/converters/video-converter/VideoConverterClient.jsx
@@ -0,0 +1,13 @@
+// src/app/tools/converters/video-converter/VideoConverterClient.jsx
+"use client";
+
+import React from "react";
+
+export default function VideoConverterClient() {
+  return (
+    <div>
+      <h2>Video Converter (client)</h2>
+      {/* your client-only UI here */}
+    </div>
+  );
+}
